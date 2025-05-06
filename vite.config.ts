ed31e1ee9c94cbb0ad904df7023bfc15071c1bd1
Solveig Årvik <soar004@student.kristiania.no>
2025-05-06 21:26:26 +0200

    fix: update metadata hashes in _metadata.json and ensure build configuration in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "dist",
+  },
   base: "/Days-Until-Moving-Out/",
   plugins: [react()],
 });
