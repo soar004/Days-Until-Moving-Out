51f5d7adb55a42c2f7c5faf4b053556623ba2781
Solveig Årvik <soar004@student.kristiania.no>
2025-05-06 21:36:13 +0200

    fix: update comments in vite.config.ts for clarity

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Days-Until-Moving-Out/", // your GitHub Pages sub-path
+  base: "/Days-Until-Moving-Out/", // your GitHub-Pages URL subpath
   build: {
-    outDir: "docs", // ← will create `/docs` instead of `/dist`
+    outDir: "docs", // write build into docs/
+    emptyOutDir: true, // remove old files in docs/ first
   },
   plugins: [react()],
 });
